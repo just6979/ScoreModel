6e9e6e1f996633947b8457adec06dfc97ddab069
Justin White <jw@justinwhite.net>
2015-08-03 23:19:26 -0400

    Create (most) tests for Game from Phase10GameTest, remove inherited methods from Phase10GameTest.

--- test/net/justinwhite/score_model/GameTest.java
+++ test/net/justinwhite/score_model/GameTest.java
@@ -34,32 +34,85 @@ package net.justinwhite.score_model;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.UUID;
+
 import static org.junit.Assert.*;
 
-public class Phase10GameTest {
+public class GameTest {
     private final int testNumPlayers;
     private final String testInitialName;
     private final String[] testPlayerNames;
     private final String testName;
-    private final Phase10Player[] testPlayersArray;
-    private Phase10Game testPhase10Game;
+    private final Player[] testPlayersArray;
+    private Game<Player> testGame;
 
     {
         testNumPlayers = 4;
         testInitialName = "P1P2P3P4";
         testPlayerNames = new String[]{"Justin W", "Lauren K", "Timmay C", "Denise B"};
         testName = "JWLKTCDB";
-        testPlayersArray = new Phase10Player[testNumPlayers];
+        testPlayersArray = new Player[testNumPlayers];
     }
 
     @Before
     public void setUp() {
-        testPhase10Game = new Phase10Game(testNumPlayers);
-        assertEquals(testInitialName, testPhase10Game.getName());
+        testGame = new Game<Player>(Player.class, testNumPlayers);
         for (int i = 0; i < testNumPlayers; i++) {
-            testPlayersArray[i] = testPhase10Game.getPlayer(i);
-            testPhase10Game.renamePlayer(String.format("Player %d", i + 1), testPlayerNames[i]);
+            testPlayersArray[i] = testGame.getPlayer(i);
         }
+    }
+
+    @Test
+    public void testToString() throws Exception {
+        assertEquals(String.format("Game: %s\nUUID: %s\nPlayer count: %d\nPlayers: %s\nPlayerMap: %s",
+                        testGame.getName(),
+                        testGame.getID(),
+                        testGame.getNumPlayers(),
+                        testGame.getPlayerList(),
+                        testGame.getPlayerMap()
+                ), testGame.toString()
+        );
+
+    }
+
+    // TODO
+    @Test
+    public void testBuildName() throws Exception {
+
+    }
+
+    // TODO
+    @Test
+    public void testSetNumPlayers() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPlayer() throws Exception {
+        assertEquals(testPlayersArray[0], testGame.getPlayer(0));
+    }
+
+    @Test
+    public void testGetPlayerByName() throws Exception {
+        assertEquals(testPlayersArray[0], testGame.getPlayerByName("Player 1"));
+    }
+
+    @Test
+    public void testCheckPlayer() throws Exception {
+        assertTrue(testGame.checkPlayer("Player 1"));
+        assertFalse(testGame.checkPlayer("Some Player"));
+    }
+
+    // TODO
+    @Test
+    public void testAddPlayer() throws Exception {
+
+    }
+
+    // TODO
+    @Test
+    public void testRenamePlayer() throws Exception {
+
     }
 
     // TODO
@@ -79,5 +132,4 @@ public class Phase10GameTest {
     public void testHasWinner() throws Exception {
 
     }
-
 }
